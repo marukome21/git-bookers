5e9891f43bd7431d3314652c23da236a49179c97
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-04-15 13:09:59 +0000

    [Add] booksのルーティング、booksコントローラーのアクション、newビュー、editビュー、showビュー

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,28 @@
+class BooksController < ApplicationController
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def delete
+    
+  end
+  
+  
+  
+end
