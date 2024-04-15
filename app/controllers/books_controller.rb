620c6a08bb0c82085166907eabc1d90d61c6f177
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-04-15 15:32:50 +0000

    [Add] indexアクション、ビュー、[Remove] newアクション、ビュー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,17 @@
 class BooksController < ApplicationController
-  def new
-    
+  def index
+    @book = Book.new
   end
   
   def create
-    
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "投稿に失敗しました。"
+      render :index
+    end
   end
   
   def show
@@ -23,6 +30,9 @@ class BooksController < ApplicationController
     
   end
   
-  
+   private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
   
 end
