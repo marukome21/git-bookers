5357418d30f88400ed74b40cd2628b37d75f53e5
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-04-15 16:30:52 +0000

    [Add] bookのcreate機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,37 +2,37 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "投稿に成功しました。"
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      flash.now[:alert] = "投稿に失敗しました。"
       render :index
     end
   end
-  
+
   def show
-    
+    @book = Book.find(params[:id])
+
   end
-  
+
   def edit
-    
+
   end
-  
+
   def update
-    
+
   end
-  
+
   def delete
-    
+
   end
-  
+
    private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
