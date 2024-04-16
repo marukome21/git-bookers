2528f4852f03e3d99238e6c87b21f4e319d2473f
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-04-16 14:18:49 +0000

    [Add] destroy機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,8 +29,10 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end
 
-  def delete
-
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
    private
